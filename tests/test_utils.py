6ba71364a3d21e0bf138f55fd4af0a96fb646c0f
dk2n16 <dkerr83@gmail.com>
2022-11-27 10:57:16 +0000

    set up test data and define initial tests

---
+++ tests/test_utils.py
@@ -0,0 +1,68 @@
+"""Unit tests for utils.py"""
+
+from pathlib import Path
+
+import building_zonals
+
+#fixtures
+
+
+def test_convert_shp_to_gpkg():
+    pass
+
+def test_open_raster():
+    pass
+
+def test_get_raster_bounds():
+    pass
+
+def test_get_buildings_using_bounds():
+    pass
+
+def test_clip_buildings_using_bounds():
+    pass
+
+def test_make_col_in_gdf_int_osm_id():
+    pass
+
+def test_rasterise_clip():
+    pass
+
+def test_save_clip_raster():
+    # OR CONVERT TO RASTERIO OBJECT
+    # IF SAVING COMPRESS FIRST
+    pass
+
+def test_make_stats_dict():
+    pass
+
+def test_zonal_stats():
+    # might need some additional functions
+    pass
+
+
+def test_make_df_from_stats_dict():
+    pass
+
+def test_combine_all_dataframes():
+    pass
+
+def test_save_df_as_csv():
+    pass
+
+def test_join_csvs_and_aggregate():
+    pass
+
+def test_find_missing_buildings():
+    pass
+
+def test_sample_missing_buildings_and_join_back_to_csv():
+    pass
+
+def test_join_csv_to_global_shape():
+    pass
+
+
+
+
+
